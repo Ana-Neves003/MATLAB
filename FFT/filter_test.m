c2c3836a08176a3002e4ac62dd7f5746759c332a
Ana-Beatriz-Marinho <anabibimn@hotmail.com>
2024-10-29 16:33:44 -0300

    Atualizando código do Matlab do filtro CIC + FIR

--- FFT/filter_test.m
+++ FFT/filter_test.m
@@ -3,70 +3,63 @@ clear;
 clc;
 
 % 1. LER O ARQUIVO DE ÁUDIO PDM (.WAV)
-filename = 'REC_SILENCIO.WAV';
+filename = 'REC_15kHz_4.WAV';
 fid = fopen(filename, 'r');
 data = fread(fid, '*uint32');
 fclose(fid);
 
 % 2. PROCESSAR O SINAL LIDO
 Nfft = size(data, 1);
-data = data(floor(Nfft/2):Nfft);
-%data = data(1:Nfft);
+data = data(floor(Nfft/2):Nfft);  % Usa metade dos dados
 binaryData = dec2bin(data) == '1';
 reorganizedData = reshape(binaryData.', 1, []);
 reorganizedData = 2*reorganizedData-1;
-Nfft = size(reorganizedData,2);
+Nfft = length(reorganizedData);
 
-% 4. PARÂMETROS DO FILTRO FIR
-fs = 5e6; % Frequência de amostragem
-fc = 90e3; % Frequência de corte do filtro FIR
-order = 64; % Ordem do filtro FIR
-N = 2; % Número de estágios CIC
-R = 16; % Taxa de decimação
-M = 1; % Atraso diferencial
+% 3. PARÂMETROS DO FILTRO CIC E FIR
+fs = 5e6;      % Frequência de amostragem
+fc = 90e3;     % Frequência de corte do FIR
+order = 64;    % Ordem do FIR
+N = 4;         % Número de estágios do CIC
+R = 16;        % Taxa de decimação do CIC
+M = 1;         % Atraso diferencial do CIC
 
 % 4. Criar o filtro CIC
-b_comb = [1 zeros(1, M-1) -1];
-b_int = ones(1, R);
-cic_filter = b_comb;
-for i = 2:N
-    cic_filter = conv(cic_filter, b_comb);
-end
-cic_filter = conv(cic_filter, b_int.^N);
+cicDecim = dsp.CICDecimator('DecimationFactor', R, 'DifferentialDelay', M, 'NumSections', N);
 
 % 5. Aplicar o filtro CIC
-cic_processed = filter(cic_filter, 1, reorganizedData);
-cic_processed = cic_processed(1:R:end); % Decimação
-fs = fs/R;
+cic_processed = cicDecim(reorganizedData.');
+fs_cic = fs / R;  % Nova frequência de amostragem após o CIC
 
-% 6. Criando o filtro FIR
-fir_coeffs = fir1(order, fc/(fs/2));
+% 6. Criar o filtro FIR
+Fc_norm = fc / (fs_cic / 2); % Frequência de corte normalizada para fs_cic
+firFilt = designfilt('lowpassfir', 'FilterOrder', order, 'CutoffFrequency', Fc_norm);
 
 % 7. Aplicar o filtro FIR
-fir_processed = filter(fir_coeffs, 1, cic_processed);
+fir_processed = filter(firFilt, cic_processed);
 
 % 8. Calcular os espectros
 Nfft_filter = length(cic_processed);
 freq = 0:fs/Nfft:fs/2;
-freq_filter = 0:fs/Nfft_filter:fs/2;
-window = hann(Nfft);
+freq_filter = 0:fs_cic/Nfft_filter:fs_cic/2;
 
 % Espectro do sinal original
-spec = fft(reorganizedData);
+spec = fft(reorganizedData, Nfft);
 spec = spec(1:Nfft/2+1);
 psd_spec = (abs(spec).^2)*(1/(fs*Nfft));
 psd_spec(2:end-1) = 2*psd_spec(2:end-1);
 
-
 % Espectro do sinal após o filtro CIC
-spec_cic = fft(cic_processed);
-psd_cic = (abs(spec_cic).^2)*(1/(fs*Nfft_filter));
-psd_cic = psd_cic(1:length(freq_filter));
+spec_cic = fft(cic_processed, Nfft_filter);
+spec_cic = spec_cic(1:Nfft_filter/2+1);
+psd_cic = (abs(spec_cic).^2)*(1/(fs_cic*Nfft_filter));
+psd_cic(2:end-1) = 2*psd_cic(2:end-1);
 
-% ESPECTRO DO SINAL APOS O FILTRO FIR
-spec_fir = fft(fir_processed);
-psd_fir = (abs(spec_fir).^2)*(1/(fs*Nfft_filter));
-psd_fir = psd_fir(1:length(freq_filter));
+% Espectro do sinal após o filtro FIR
+spec_fir = fft(fir_processed, Nfft_filter);
+spec_fir = spec_fir(1:Nfft_filter/2+1);
+psd_fir = (abs(spec_fir).^2)*(1/(fs_cic*Nfft_filter));
+psd_fir(2:end-1) = 2*psd_fir(2:end-1);
 
 % VISUALIZAÇÃO
 figure;
@@ -79,12 +72,14 @@ ylabel('Potência (dB/Hz)');
 
 subplot(3, 1, 2);
 semilogx(freq_filter, pow2db(psd_cic));
+grid on;
 title('Espectro após o Filtro CIC');
 xlabel('Frequência (Hz)');
 ylabel('Potência (dB/Hz)');
 
 subplot(3, 1, 3);
 semilogx(freq_filter, pow2db(psd_fir));
+grid on;
 title('Espectro do Filtro CIC + FIR');
 xlabel('Frequência (Hz)');
 ylabel('Potência (dB/Hz)');
